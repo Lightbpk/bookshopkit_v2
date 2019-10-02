8555a0cd8d15a966373357053cdf4d02e09f27a7
Lightbpk <bpk395@mail.ru>
2019-10-02 18:12:02 +0700

    handler ne dorabotan

--- app/src/main/java/com/example/bookshopkit_v2/Book.java
+++ app/src/main/java/com/example/bookshopkit_v2/Book.java
@@ -1,5 +1,9 @@
 package com.example.bookshopkit_v2;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -10,27 +14,38 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Book {
     private static final String LL = "LightLog";
     private FirebaseDatabase database = FirebaseDatabase. getInstance ();
     DatabaseReference booksRef = database.getReference ( "booksBase" );
     private int ID;
-    private boolean loadComplite= false;
-    private DataSnapshot lastSnap;
+    private DataSnapshot lastBookBaseSnap;
+    volatile boolean loadComplite= false;
     private String name="NoName",
-            preview = "https://firebasestorage.googleapis.com/v0/b/bookshopkitv2.appspot.com/o/previews%2FEmpty.jpg?alt=media&token=9d54db17-4383-4a96-8ae2-d6ce369e1603",
+            previewUrl = "https://firebasestorage.googleapis.com/v0/b/bookshopkitv2.appspot.com/o/previews%2FEmpty.jpg?alt=media&token=9d54db17-4383-4a96-8ae2-d6ce369e1603",
             author = "NoAuthor",
             note = "no note",
             type = "free",
             file = "empty.zip";
+    Context context;
+
     public Book(int ID) {
         this.ID = ID;
-        booksRef.child("b"+ID).addValueEventListener(new ValueEventListener() {
+        //Fireget fireget = new Fireget();
+        //fireget.start();
+        booksRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                lastSnap = dataSnapshot;
+                lastBookBaseSnap = dataSnapshot;
                 Log.d(LL,"Snap granted");
                 loadComplite = true;
+
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
@@ -38,49 +53,89 @@ public class Book {
                 loadComplite = false;
             }
         }) ;
+        /*try {
+            fireget.join();
+            Log.d(LL,"Waiting 1sec");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Log.d(LL,"Interrupt exception");
+        }*/
     }
+    class Fireget extends Thread{
+
+        @Override public void run() {
+
+        }
+        boolean isComplite(){
+            return loadComplite;
+        }
+    }
+
     public int getID () {
             return ID;
         }
-    public boolean isLoadComplite(){
-        return loadComplite;
-    }
 
     public String getName(){
-        if(lastSnap.hasChild("Name")) {
-            name = "" + lastSnap.child("Name").getValue();
+        if(lastBookBaseSnap.hasChild("Name")) {
+            name = "" + lastBookBaseSnap.child("Name").getValue();
         }
         return name;
 
     }
-    public String getPreview(){
-        if(lastSnap.hasChild("Preview")) {
-            preview = "" + lastSnap.child("Preview").getValue();
+    public String getPreviewUrl(){
+        if(lastBookBaseSnap.hasChild("Preview")){
+            previewUrl = ""+ lastBookBaseSnap.child("Preview").getValue();
         }
-        return preview;
+        return previewUrl;
+
     }
+    public Bitmap getPreviewBitmap(Context context){
+        Bitmap bm = null;
+
+        this.context = context;
+        if(lastBookBaseSnap != null) {
+            if (lastBookBaseSnap.child("b0").hasChild("Preview")) {
+                previewUrl = "" + lastBookBaseSnap.child("Preview").getValue();
+                try {
+                    URL aURL = new URL(previewUrl);
+                    URLConnection conn = aURL.openConnection();
+                    conn.connect();
+                    InputStream is = conn.getInputStream();
+                    BufferedInputStream bis = new BufferedInputStream(is);
+                    bm = BitmapFactory.decodeStream(bis);
+                    bis.close();
+                    is.close();
+                } catch (IOException e) {
+                    Log.d(LL, "Error getting bitmap", e);
+                }
+            }
+        }else bm = BitmapFactory.decodeResource(context.getResources(),R.drawable.empty
+                );
+        return bm;
+    }
+
     public String getAuthor(){
-        if(lastSnap.hasChild("Author")){
-            author = ""+lastSnap.child("Author").getValue();
+        if(lastBookBaseSnap.hasChild("Author")){
+            author = ""+ lastBookBaseSnap.child("Author").getValue();
         }
         return author;
     }
 
     public String getNote(){
-        if(lastSnap.hasChild("Note")){
-            note = ""+lastSnap.child("Note").getValue();
+        if(lastBookBaseSnap.hasChild("Note")){
+            note = ""+ lastBookBaseSnap.child("Note").getValue();
         }
         return  note;
     }
     public String getType(){
-        if(lastSnap.hasChild("Type")){
-            type = ""+lastSnap.child("Type").getValue();
+        if(lastBookBaseSnap.hasChild("Type")){
+            type = ""+ lastBookBaseSnap.child("Type").getValue();
         }
         return type;
     }
     public String getFile(){
-        if(lastSnap.hasChild("File")){
-            file = ""+lastSnap.child("File").getValue();
+        if(lastBookBaseSnap.hasChild("File")){
+            file = ""+ lastBookBaseSnap.child("File").getValue();
         }
         return file;
     }
--- app/src/main/java/com/example/bookshopkit_v2/BookListActivity.java
+++ app/src/main/java/com/example/bookshopkit_v2/BookListActivity.java
@@ -1,30 +1,95 @@
 package com.example.bookshopkit_v2;
 
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class BookListActivity extends Activity {
+public class BookListActivity extends Activity  {
 final String LL ="LightLog";
+int num_books_v = 3;
+int num_books_h = 3;
+int num_books_visible = num_books_h * num_books_v;
+int startID = 0;
+TableLayout book_list_lay;
+ImageView[] booksImageViews = new ImageView[num_books_visible];
+Book[] books = new Book[num_books_visible];
+ImageView[] imageViews = new ImageView[num_books_visible];
+TextView tw;
+Book book;
+Handler h;
 
+final int STATUS_OK = 1;
+final int STATUS_NON = 0;
+
+    @SuppressLint("HandlerLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //---------полноэкран----------
+        //---------Fullscreen activity----------
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_book_list);
         //------------------------------
-        Book book = new Book(1);
-        ViewDraw viewDraw = new ViewDraw(book);
-        viewDraw.start();
+        book_list_lay = findViewById(R.id.book_list_lay);
+        bookListUpdate();
+        h = new Handler(){
+            public void handleMessage(android.os.Message msg){
+
+                if(msg.what == STATUS_OK){
+                    tw.setText(book.getName());
+                    book_list_lay.addView(tw);
+                    Log.d(LL,"STATUS_OK");
+                }else if(msg.what == STATUS_NON){
+                    tw.setText("nononon");
+                    book_list_lay.addView(tw);
+                    Log.d(LL,"STATUS_NON");
+                }else Log.d(LL,"STATUS_undefine");
+
+            }
+        };
+        h.sendEmptyMessage(STATUS_NON);
+    }
+    public void bookListUpdate(){
+
+        book = new Book(0);
+        WaitData waitdata = new WaitData();
+        waitdata.start();
+      /*for(int i = 0 ; i < num_books_v ; i++){
+           TableRow row = new TableRow(this);
+           for(int j =0 ; j < num_books_h ; j++){
+               books[startID] =  new Book(startID);
+               imageViews[startID].setImageBitmap(books[startID].getPreviewBitmap(this));
+               row.addView(imageViews[startID]);
+           }
+           book_list_lay.addView(row);
+       }*/
+        //book_list_lay.addView();
+    }
+
+    public void onDrawBook(String text){
+
+
+    }
+    class WaitData extends Thread{
+        @Override public void run() {
+            while (!book.loadComplite){
+            Log.d(LL,"Loading");
+            }
+            h.sendEmptyMessage(STATUS_OK);
+        }
     }
 
 
