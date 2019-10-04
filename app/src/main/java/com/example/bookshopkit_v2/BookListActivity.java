4314a4895d54ce35ceb083f6fa60431995985d7e
Lightbpk <bpk395@mail.ru>
2019-10-04 13:10:59 +0700

    list book updating dinamyc books add -start test

--- app/src/main/java/com/example/bookshopkit_v2/BookListActivity.java
+++ app/src/main/java/com/example/bookshopkit_v2/BookListActivity.java
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
@@ -16,6 +17,8 @@ import android.widget.TextView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 public class BookListActivity extends Activity  {
 final String LL ="LightLog";
 int num_books_v = 3;
@@ -23,11 +26,12 @@ int num_books_h = 3;
 int num_books_visible = num_books_h * num_books_v;
 int startID = 0;
 TableLayout book_list_lay;
+TableRow firstrow;
 ImageView[] booksImageViews = new ImageView[num_books_visible];
-Book[] books = new Book[num_books_visible];
-ImageView[] imageViews = new ImageView[num_books_visible];
+TextView[] textViews = new TextView[num_books_visible];
 TextView tw;
-Book book;
+
+//ArrayList booksAL = new ArrayList();
 Handler h;
 
 final int STATUS_OK = 1;
@@ -44,15 +48,19 @@ final int STATUS_NON = 0;
         setContentView(R.layout.activity_book_list);
         //------------------------------
         book_list_lay = findViewById(R.id.book_list_lay);
+        firstrow = findViewById(R.id.firstRow);
         tw = findViewById(R.id.tw);
         bookListUpdate();
+        //-----------Draw Book-------------
         h = new Handler(){
             public void handleMessage(android.os.Message msg){
-
                 if(msg.what == STATUS_OK){
-                    tw.setText(book.getName());
-                    tw.invalidate();
-                    Log.d(LL,"STATUS_OK"+" "+book.getName());
+                    Book currentBook = (Book)msg.obj;
+                    textViews[currentBook.getID()]= new TextView(BookListActivity.this);
+                    textViews[currentBook.getID()].setText(currentBook.getName());
+                    firstrow.addView(textViews[currentBook.getID()]);
+                    firstrow.invalidate();
+                    Log.d(LL,"STATUS_OK"+" "+currentBook.getName());
                 }else if(msg.what == STATUS_NON){
                     tw.setText("nononon");
                     tw.invalidate();
@@ -61,13 +69,17 @@ final int STATUS_NON = 0;
 
             }
         };
+        //------------------------------
         h.sendEmptyMessage(STATUS_NON);
     }
     public void bookListUpdate(){
+        for(int i = 0 ; i < num_books_visible ; i++){
+            Book currentBook = new Book(i);
+            WaitData waitdata = new WaitData(currentBook);
+            waitdata.start();
+        }
 
-        book = new Book(0);
-        WaitData waitdata = new WaitData();
-        waitdata.start();
+
       /*for(int i = 0 ; i < num_books_v ; i++){
            TableRow row = new TableRow(this);
            for(int j =0 ; j < num_books_h ; j++){
@@ -85,14 +97,17 @@ final int STATUS_NON = 0;
 
     }
     class WaitData extends Thread{
+        Book currentBook;
+        public WaitData(Book currentBook){
+            this.currentBook = currentBook;
+        }
         @Override public void run() {
-            while (!book.loadComplite){
+            Message msg;
+            while (!currentBook.loadComplite){
             Log.d(LL,"Loading");
             }
-
-            h.sendEmptyMessage(STATUS_OK);
+            msg = h.obtainMessage(STATUS_OK,currentBook);
+            h.sendMessage(msg);
         }
     }
-
-
 }
