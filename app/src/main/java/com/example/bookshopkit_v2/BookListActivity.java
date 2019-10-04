42f5226ea004026b3e76f39593631c4c1026f0c5
Lightbpk <bpk395@mail.ru>
2019-10-04 15:30:38 +0700

    error index 3 size 1

--- app/src/main/java/com/example/bookshopkit_v2/BookListActivity.java
+++ app/src/main/java/com/example/bookshopkit_v2/BookListActivity.java
@@ -14,6 +14,8 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class BookListActivity extends Activity  {
 final String LL ="LightLog";
 int BOOK_VIEW_V = 3;
@@ -23,10 +25,9 @@ int startID = 0;
 TableLayout book_list_lay;
 TableRow firstrow;
 ImageView[] booksImageViews = new ImageView[BOOK_VIEW];
-TextView[] textViews = new TextView[BOOK_VIEW];
+ArrayList<TextView>  textViews = new ArrayList<TextView>();
 TextView tw;
 
-//ArrayList booksAL = new ArrayList();
 Handler h;
 
 final int STATUS_OK = 1;
@@ -51,9 +52,11 @@ final int STATUS_NON = 0;
             public void handleMessage(android.os.Message msg){
                 if(msg.what == STATUS_OK){
                     Book currentBook = (Book)msg.obj;
-                    textViews[currentBook.getID()]= new TextView(BookListActivity.this);
-                    textViews[currentBook.getID()].setText(currentBook.getName());
-                    firstrow.addView(textViews[currentBook.getID()]);
+                    TextView currentText;
+                    textViews.add(new TextView(BookListActivity.this));
+                    currentText = textViews.get(msg.arg1);
+                    currentText.setText(currentBook.getName());
+                    firstrow.addView(currentText);
                     firstrow.invalidate();
                     Log.d(LL,"STATUS_OK"+" "+currentBook.getName());
                 }else if(msg.what == STATUS_NON){
@@ -68,39 +71,26 @@ final int STATUS_NON = 0;
     }
     public void bookListUpdate(int startID){
         for(int i = 0; i < BOOK_VIEW; i++){
+            int current_img_num= i;
             Book currentBook = new Book(i+startID);
-            WaitData waitdata = new WaitData(currentBook);
+            WaitData waitdata = new WaitData(currentBook, current_img_num);
             waitdata.start();
         }
-
-
-      /*for(int i = 0 ; i < BOOK_VIEW_V ; i++){
-           TableRow row = new TableRow(this);
-           for(int j =0 ; j < BOOK_VIEW_H ; j++){
-               books[startID] =  new Book(startID);
-               imageViews[startID].setImageBitmap(books[startID].getPreviewBitmap(this));
-               row.addView(imageViews[startID]);
-           }
-           book_list_lay.addView(row);
-       }*/
-        //book_list_lay.addView();
     }
 
-    public void onDrawBook(String text){
-
-
-    }
     class WaitData extends Thread{
         Book currentBook;
-        public WaitData(Book currentBook){
+        int current_img_num;
+        public WaitData(Book currentBook,int current_img_num){
             this.currentBook = currentBook;
+            this.current_img_num = current_img_num;
         }
         @Override public void run() {
             Message msg;
             while (!currentBook.loadComplite){
             //Log.d(LL,"Loading");
             }
-            msg = h.obtainMessage(STATUS_OK,currentBook);
+            msg = h.obtainMessage(STATUS_OK,current_img_num,0,currentBook);
             h.sendMessage(msg);
         }
     }
